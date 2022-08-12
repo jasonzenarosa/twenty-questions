e1ee7321b353a74857b8fe10b5a2e3b6ac48d451
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-08-11 21:09:48 -0700

    Added game functionality

--- guest.py
+++ guest.py
@@ -4,9 +4,11 @@ class Guest:
 
     def __init__(self, host:str, port:int, player:str):
         self.player = player
+        self.guesses_left = 20
         self.host = host
         self.port = port
         self.join_game()
+
 
     def join_game(self):    
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -14,8 +16,34 @@ class Guest:
             self.opponent = s.recv(4096).decode('utf-8')
             s.send(self.player.encode('utf-8'))
             print(f"You have joined {self.opponent}'s game!")
+            self.game(s)
     
 
+    def game(self, s:socket.socket):
+        print(f"{self.opponent}'s turn...")
+        s.recv(4096)
+        while self.guesses_left > 0:
+            s.send(self.guess().encode())
+            print(f"{self.opponent}'s turn...")
+            response = s.recv(4096).decode('utf-8')
+            if response == 'Y':
+                print('Yes!')
+            elif response == 'N':
+                print('No!')
+            elif response == 'S':
+                print('Sometimes!')
+            elif response == 'C':
+                print('You got it! Thanks for playing!')
+                break
+        else:
+            print('You are out of guesses. Thanks for playing!')
+
+
+    def guess(self):
+        guess = input('Ask a question or enter a guess!\n')
+        return guess
+
+
 if __name__ == '__main__':
-    Guest('127.0.0.1', 2020, 'kaitlen')
+    Guest('127.0.0.1', 2022, 'kaitlen')
             
--- host.py
+++ host.py
@@ -12,16 +12,53 @@ class Host:
         self.port = port
         self.make_game()
 
+
     def make_game(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.host, self.port))
             s.listen()
+            
             print('Waiting for connection...')
             conn, addr = s.accept()
+            
             conn.send(self.player.encode('utf-8')) # send player username
             self.opponent = conn.recv(4096).decode('utf-8') # receive opponent username
             print(f'{self.opponent} has joined your game!')
+            self.game(input('Enter a word!\n'), conn)
+    
+
+    def game(self, secret:str, conn:socket.socket):
+        conn.send(secret.encode('utf-8'))
+        while self.guesses_left > 0:
+            print(f"{self.opponent}'s turn...")
+            guess = conn.recv(4096).decode('utf-8')
+            self.guesses_left -= 1
+            if guess == secret:
+                print(f'{self.opponent} has guessed your word. Thanks for playing!')
+                conn.send(b'C')
+                break
+            else:
+                if guess[-1] == '?':
+                    print(guess)
+                else:
+                    print(f'{guess}?')
+                response = self.response()
+                conn.send(response.encode('utf-8'))
+                if response == 'C':
+                    print('Thanks for playing!')
+                    break
+        else:
+            print(f'{self.opponent} has run out of guesses. Thanks for playing!')
+            
+
+    def response(self):
+        while True:
+            user_input = input('What is your response? (y - yes, n - no, s - sometimes, c - correct answer)\n').upper()
+            if user_input in ['Y', 'N', 'S', 'C']:
+                return user_input
+            else:
+                print('Invalid input. Try again!')
 
 
 if __name__ == '__main__':
-    Host(True, 2020, 'jason')
+    Host(True, 2022, 'jason')
